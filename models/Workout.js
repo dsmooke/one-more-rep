97ed51d5feaa839b0d64356789ffb45a64e66ec8
Danny Smooke <dana.smooke@gmail.com>
2021-02-27 14:08:16 -0500

    created models folder

--- models/Workout.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose)";
-
-const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema({
-    
-});
-
-https://github.com/kqarlos/fitness-tracker/blob/master/models/Workout.js
