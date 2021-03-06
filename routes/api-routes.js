916be33c85bdf42c0ac45a376147da6be19b1391
Danny Smooke <dana.smooke@gmail.com>
2021-03-06 17:30:22 -0500

    added get request for getting workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,4 +14,27 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+// Get Workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      // console.log("Displaying all workouts.");
+      // console.log(dbWorkout);
+      dbWorkout.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((e) => {
+          total += e.duration;
+        });
+
+        workout.totalDuration = total;
+      });
+
+      res.json(dbWorkout);
+    })
+
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
