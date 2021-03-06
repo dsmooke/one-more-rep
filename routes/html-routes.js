0f7b880e13d5b7af6236b6e48e61881a95d19de6
Danny Smooke <dana.smooke@gmail.com>
2021-03-06 17:35:54 -0500

    added three get requests: index, exercise, and stats html files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
 const path = require("path");
 
+// display main view on "fitness tracker"
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// display exercises on "new workout"
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+// display stats page "dashboard"
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
 module.exports = router;
