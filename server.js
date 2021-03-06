36a01ad583377bcd6707ac67dbf8e7ebdb6f0222
Danny Smooke <dana.smooke@gmail.com>
2021-03-06 17:41:44 -0500

    added route paths to routes/api-routes and routes/html-routes; review file paths and pluralizations - check

--- server.js
+++ server.js
@@ -15,9 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useFindAndModify: false,
 });
+
+// Routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 // Start the server
 app.listen(PORT, () => {
