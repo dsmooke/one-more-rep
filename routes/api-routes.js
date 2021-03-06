9ac5dfacdea12f096509a9899bb45001227eacec
Danny Smooke <dana.smooke@gmail.com>
2021-03-06 17:29:40 -0500

    added post request for creating workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,17 @@
 const db = require("../models/Workout.js");
 const router = require("express").Router();
 
+// Create Workout
+router.post("/api/workouts", ({ body }, res) => {
+  // console.log("Adding workout...");
+  // console.log(body);
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
