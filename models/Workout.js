f58749771a22070c6b6f7d5d8dd7a2b0b30dbe35
Danny Smooke <dana.smooke@gmail.com>
2021-03-06 17:37:12 -0500

    created Workout model schema, exported as Workout

---
+++ models/Workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        // required: "Enter a type of exercise",
+      },
+      name: {
+        type: String,
+        trim: true,
+        // required: "Enter a type of exercise",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
+    },
+  ],
+
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
