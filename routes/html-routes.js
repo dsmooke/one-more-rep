6844cae92a6344404abafa91ffb6da0698725769
Danny Smooke <dana.smooke@gmail.com>
2021-03-08 19:10:00 -0500

    removed inline bookmarks

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
-const path = require("path"); // @audit-issue causing routes/exercise.html
+const path = require("path");
 
 // display main view on "fitness tracker"
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html")); // @audit shortened path
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // display exercises on "new workout"
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html")); // @audit shortened path
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 // display stats page "dashboard"
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html")); // @audit shortened path
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
