046100b33b87b181669bdcc4bef08b5ff0858c95
Danny Smooke <dana.smooke@gmail.com>
2021-03-08 19:09:17 -0500

    finally corrected path to public-html files - success

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
-const path = require("path");
+const path = require("path"); // @audit-issue causing routes/exercise.html
 
 // display main view on "fitness tracker"
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html")); // @audit shortened path
 });
 
 // display exercises on "new workout"
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html")); // @audit shortened path
 });
 
 // display stats page "dashboard"
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats.html")); // @audit shortened path
 });
 
 module.exports = router;
